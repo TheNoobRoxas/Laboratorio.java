3f85c967e3d6f33d67b4acc92e22bb23b819503f
TheNoobRoxas <nazem.navid@gmail.com>
2022-04-11 18:27:04 +0200

    Creazione metodo equals.

--- src/emotionalsongs/Brano.java
+++ src/emotionalsongs/Brano.java
@@ -1,14 +1,29 @@
 package emotionalsongs;
 
+import java.util.Objects;
+
 public class Brano {
-    public String Titolo;
-    public String Autore;
+    public String titolo;
+    public String autore;
     public int anno;
 
-    public Brano(String Titolo, String Autore, int anno) {
-        this.Titolo=Titolo;
-        this.Autore=Autore;
+    public Brano(String titolo, String autore, int anno) {
+        this.titolo=titolo;
+        this.autore=autore;
         this.anno=anno;
+    }
+
+    public boolean equals(Brano brano) {
+        return Objects.equals(this.titolo, brano.titolo) && Objects.equals(this.autore, brano.autore) && this.anno == brano.anno;
+    }
+
+    public boolean equals(String titolo) {
+        return (Objects.equals(this.titolo, titolo));
+
+    }
+
+    public boolean equals(String autore,int anno) {
+        return (Objects.equals(this.autore, autore) && this.anno == anno);
     }
 
 }
